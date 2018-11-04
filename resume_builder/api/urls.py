1362224cbf82be8fc6fec29b33468afcca67c292
pbdgk <gordonworkspace@gmail.com>
2018-11-04 23:43:15 +0200

    created endpoing for job update

--- resume_builder/api/urls.py
+++ resume_builder/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProfileRestView
+from .views import ProfileRestView, JobRestView
 
 urlpatterns = [
-    path('profile/', ProfileRestView.as_view(), name='profile')
+    path('profile/', ProfileRestView.as_view(), name='profile'),
+    path('jobs/<int:pk>/', JobRestView.as_view(), name='job'),
 ]
