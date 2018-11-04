9f2fc54ce05ebaafea1fab91570f0d76012ffd13
pbdgk <gordonworkspace@gmail.com>
2018-11-04 23:46:59 +0200

    start to create rest views; registered model in admin panel

--- resume_builder/api/views.py
+++ resume_builder/api/views.py
@@ -5,8 +5,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
 
-from app.models import Profile
-from app.serializers import ProfileSerializer
+from app.models import Profile, Job
+from app.serializers import ProfileSerializer, JobSerializer
 
 
 class ProfileRestView(APIView):
@@ -14,14 +14,35 @@ class ProfileRestView(APIView):
 
     def put(self, request, format=None):
         profile = Profile.objects.get(user=request.user)
-        data = ProfileSerializer(profile, data=request.data, partial=True)
-        if data.is_valid():
-            data.save()
-            return Response(data, status=status.HTTP_200_OK)
-        return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
+        serialized = ProfileSerializer(profile, data=request.data, partial=True)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, format=None):
         profile = Profile.objects.get(user=request.user)
-        if profile:
-            return Response(profile, status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        serialized = ProfileSerializer(profile)
+        if serialized:
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        return Response(serialized.errors, status=status.HTTP_404_NOT_FOUND)
+
+
+class JobRestView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self, request, pk, format=None):
+        # TODO: ITS wrong! another logic retreiving of job!
+        job = Job.objects.filter(user=request.user)[pk]
+        serialized = JobSerializer(job, data=request.data, partial=True)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, format=None):
+        job = Job.objects.filter(user=request.user)
+        serialized = JobSerializer(job, many=True)
+        if serialized:
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        return Response(serialized.errors, status=status.HTTP_404_NOT_FOUND)
