480839ebc96fbbb8be3f86895410acb1b64d0536
pbdgk <gordonworkspace@gmail.com>
2018-11-04 23:44:12 +0200

    start to create  resume models and serializers

--- resume_builder/app/models.py
+++ resume_builder/app/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+# class Photo(models.Model):
+#     owner = models.ForeignKey('auth.User', related_name='image')
+#     image = models.ImageField(upload_to='photos')
+
+
+# class ResumeData(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     profile =
+
+class Profile(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    profession = models.CharField(max_length=255, blank=True)
+
+    # presonal_data = models.ForeignKey("PersonalData", on_delete=models.CASCADE)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def build_profile_on_user_creation(sender, instance, created, **kwargs):
+  if created:
+    profile = Profile(user=instance)
+    profile.save()
+
+
+# class PersonalData(models.Model):
+#     user = models.ManyToManyField(settings.AUTH_USER_MODEL)
+#     phone = models.CharField(max_length=18, blank=True, default="")
+#     email = models.EmailField(blank=True, default="")
+#     birthday = models.DateField(blank=True, default="")
+
+
+# class SocialData(models.Model):
+#     user = models.ManyToManyField(settings.AUTH_USER_MODEL)
+#     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+#     img_class = models.CharField(max_length=255)
+#     link = models.URLField(max_length=255, blank=True, default="")
+#
+#
+class Job(models.Model):
+    user = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    position = models.CharField(max_length=255, blank=True, default="")
+    company = models.CharField(max_length=255, blank=True, default="")
+    start_time = models.DateField(blank=True, null=True)
+    end_time = models.DateField(blank=True, null=True)
+    experience = models.TextField(blank=True, default="")
+#
+# class Languange(models.Model):
+#     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+#     name = models.CharField(max_length=255)
+#     stars = models.IntegerField()
+#
+# class Skill(models.Model):
+#     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+#     name = models.CharField(max_length=255)
+#     stars = models.IntegerField()
---
+++ resume_builder/app/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Profile, Job
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('first_name', 'last_name', 'profession')
+
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = ('position', 'company', 'start_time', 'end_time', 'experience',)
+
+
+# class PersonalDataSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model PersonalData
+#         fields = ('')
+# class PhotoSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Photo
+#         fields = ('image', 'owner')
+#
+#         # TODO: check this down
+#         owner = serializers.Field(source='owner.username')
+
+# class ResumeSerializer(serializers.Serializer):
+
+    # main = MainSerializer()
+    # photo = PhotoSerializer()
+    # personal = PersonalSerializer(many=True)
+    # social = SocialSerializer(many=True)
+    # jobs = JobSerializer(many=True)
+    # skills = SkillSerializer(many=True)
+    # languages = LangunageSerializer(many=True)
