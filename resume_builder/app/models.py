63f1ebd5a9f50b04ce40e11d0c5f9d6b82ebdcfe
pbdgk <gordonworkspace@gmail.com>
2018-11-06 23:42:24 +0200

    created school model

--- resume_builder/app/models.py
+++ resume_builder/app/models.py
@@ -4,15 +4,6 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 
-# class Photo(models.Model):
-#     owner = models.ForeignKey('auth.User', related_name='image')
-#     image = models.ImageField(upload_to='photos')
-
-
-# class ResumeData(models.Model):
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     profile =
-
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255, blank=True)
@@ -26,26 +17,6 @@ def build_profile_on_user_creation(sender, instance, created, **kwargs):
         Profile.objects.get_or_create(user=instance)
 
 
-
-
-# class PersonalData(models.Model):
-#     user = models.ManyToManyField(settings.AUTH_USER_MODEL)
-#     phone = models.CharField(max_length=18, blank=True, default="")
-#     email = models.EmailField(blank=True, default="")
-#     birthday = models.DateField(blank=True, default="")
-
-
-# class SocialData(models.Model):
-#     user = models.ManyToManyField(settings.AUTH_USER_MODEL)
-#     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-#     img_class = models.CharField(max_length=255)
-#     link = models.URLField(max_length=255, blank=True, default="")
-#
-#
-
-
-
-
 class Job(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     position = models.CharField(max_length=255, blank=True, default="")
@@ -55,10 +26,8 @@ class Job(models.Model):
     experience = models.TextField(blank=True, default="")
     priority = models.IntegerField(blank=True)
 
-
     def save(self, *args, **kwargs):
         if self.priority is None:
-            print(self.user)
             jobs = Job.objects.filter(user=self.user)
             if not jobs.exists():
                 self.priority = 1
@@ -76,6 +45,56 @@ class Job(models.Model):
                 job.priority = job.priority - 1
                 job.save()
 
+
+class School(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True, default="")
+    start = models.DateField(blank=True, null=True)
+    end = models.DateField(blank=True, null=True)
+    description = models.TextField(blank=True, default="")
+    priority = models.IntegerField(blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.priority is None:
+            objects = School.objects.filter(user=self.user)
+            if not objects.exists():
+                self.priority = 1
+            else:
+                self.priority = objects.count() + 1
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        priority = self.priority
+        user = self.user
+        super().delete(*args, **kwargs)
+        objects = School.objects.filter(user=user)
+        for obj in objects:
+            if obj.priority > priority:
+                obj.priority = obj.priority - 1
+                obj.save()
+
+
+# class Photo(models.Model):
+#     owner = models.ForeignKey('auth.User', related_name='image')
+#     image = models.ImageField(upload_to='photos')
+
+
+# class ResumeData(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     profile =
+
+# class PersonalData(models.Model):
+#     user = models.ManyToManyField(settings.AUTH_USER_MODEL)
+#     phone = models.CharField(max_length=18, blank=True, default="")
+#     email = models.EmailField(blank=True, default="")
+#     birthday = models.DateField(blank=True, default="")
+
+
+# class SocialData(models.Model):
+#     user = models.ManyToManyField(settings.AUTH_USER_MODEL)
+#     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+#     img_class = models.CharField(max_length=255)
+#     link = models.URLField(max_length=255, blank=True, default="")
 
 # class Languange(models.Model):
 #     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
