088746aac1828fc4d884d72d8e8b6d1041cd8cd0
pbdgk <gordonworkspace@gmail.com>
2018-11-06 20:54:12 +0200

    created delete put and get views for job model

--- resume_builder/api/views.py
+++ resume_builder/api/views.py
@@ -31,9 +31,27 @@ class ProfileRestView(APIView):
 class JobRestView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def put(self, request, pk, format=None):
-        # TODO: ITS wrong! another logic retreiving of job!
-        job = Job.objects.filter(user=request.user)[pk]
+    def delete(self, request, format=None):
+        user = request.user
+        priority = request.POST.get('priority')
+        job = Job.objects.filter(user=user).filter(priority=priority)[0]
+        job = job.delete()
+        serialized = JobSerializer(job)
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+
+    def post(self, request, format=None):
+        user = request.user
+        job = Job.objects.create(user=user)
+        serialized = JobSerializer(job)
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+
+    def put(self, request, format=None):
+        priority = request.POST.get('priority')
+        print(request.POST)
+        st = request.POST.get("start")
+        print(st)
+        job = Job.objects.filter(user=request.user).filter(priority=priority)[0]
         serialized = JobSerializer(job, data=request.data, partial=True)
         if serialized.is_valid():
             serialized.save()
@@ -41,8 +59,8 @@ class JobRestView(APIView):
         return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, format=None):
-        job = Job.objects.filter(user=request.user)
-        serialized = JobSerializer(job, many=True)
+        jobs = Job.objects.filter(user=request.user)
+        serialized = JobSerializer(jobs, many=True)
         if serialized:
             return Response(serialized.data, status=status.HTTP_200_OK)
         return Response(serialized.errors, status=status.HTTP_404_NOT_FOUND)
