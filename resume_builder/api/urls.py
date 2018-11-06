335dab125ce95ea49692f6be3a4833136c33b98a
pbdgk <gordonworkspace@gmail.com>
2018-11-06 20:48:29 +0200

    change api endpoint for jobs

--- resume_builder/api/urls.py
+++ resume_builder/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from .views import ProfileRestView, JobRestView
 
 urlpatterns = [
     path('profile/', ProfileRestView.as_view(), name='profile'),
-    path('jobs/<int:pk>/', JobRestView.as_view(), name='job'),
+    path('jobs/', JobRestView.as_view(), name='job'),
 ]
