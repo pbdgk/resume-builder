af695f445507fcb471681830254776ae4dfaeebb
pbdgk <gordonworkspace@gmail.com>
2018-11-06 23:42:56 +0200

    created school api endpoint and associated crud view

--- resume_builder/api/urls.py
+++ resume_builder/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ProfileRestView, JobRestView
+from .views import ProfileRestView, JobRestView, SchoolRestView
+
 
 urlpatterns = [
     path('profile/', ProfileRestView.as_view(), name='profile'),
     path('jobs/', JobRestView.as_view(), name='job'),
+    path('schools/', SchoolRestView.as_view(), name='school'),
 ]
--- resume_builder/api/views.py
+++ resume_builder/api/views.py
@@ -5,12 +5,19 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
 
-from app.models import Profile, Job
-from app.serializers import ProfileSerializer, JobSerializer
+from app.models import Profile, Job, School
+from app.serializers import ProfileSerializer, JobSerializer, SchoolSerializer
 
 
 class ProfileRestView(APIView):
     permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        profile = Profile.objects.get(user=request.user)
+        serialized = ProfileSerializer(profile)
+        if serialized:
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        return Response(serialized.errors, status=status.HTTP_404_NOT_FOUND)
 
     def put(self, request, format=None):
         profile = Profile.objects.get(user=request.user)
@@ -20,43 +27,9 @@ class ProfileRestView(APIView):
             return Response(serialized.data, status=status.HTTP_200_OK)
         return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def get(self, request, format=None):
-        profile = Profile.objects.get(user=request.user)
-        serialized = ProfileSerializer(profile)
-        if serialized:
-            return Response(serialized.data, status=status.HTTP_200_OK)
-        return Response(serialized.errors, status=status.HTTP_404_NOT_FOUND)
-
 
 class JobRestView(APIView):
     permission_classes = [permissions.IsAuthenticated]
-
-    def delete(self, request, format=None):
-        user = request.user
-        priority = request.POST.get('priority')
-        job = Job.objects.filter(user=user).filter(priority=priority)[0]
-        job = job.delete()
-        serialized = JobSerializer(job)
-        return Response(serialized.data, status=status.HTTP_200_OK)
-
-
-    def post(self, request, format=None):
-        user = request.user
-        job = Job.objects.create(user=user)
-        serialized = JobSerializer(job)
-        return Response(serialized.data, status=status.HTTP_201_CREATED)
-
-    def put(self, request, format=None):
-        priority = request.POST.get('priority')
-        print(request.POST)
-        st = request.POST.get("start")
-        print(st)
-        job = Job.objects.filter(user=request.user).filter(priority=priority)[0]
-        serialized = JobSerializer(job, data=request.data, partial=True)
-        if serialized.is_valid():
-            serialized.save()
-            return Response(serialized.data, status=status.HTTP_200_OK)
-        return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, format=None):
         jobs = Job.objects.filter(user=request.user)
@@ -64,3 +37,54 @@ class JobRestView(APIView):
         if serialized:
             return Response(serialized.data, status=status.HTTP_200_OK)
         return Response(serialized.errors, status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request, format=None):
+        job = Job.objects.create(user=request.user)
+        serialized = JobSerializer(job)
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+
+    def put(self, request, format=None):
+        priority = request.POST.get('priority')
+        job = Job.objects.filter(user=request.user).filter(priority=priority)[0]
+        serialized = JobSerializer(job, data=request.data, partial=True)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, format=None):
+        priority = request.POST.get('priority')
+        job = Job.objects.filter(user=request.user).filter(priority=priority)[0]
+        serialized = JobSerializer(job.delete())
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+
+class SchoolRestView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        schools = School.objects.filter(user=request.user)
+        serialized = SchoolSerializer(schools, many=True)
+        if serialized:
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        return Response(serialized.errors, status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request, format=None):
+        job = School.objects.create(user=request.user)
+        serialized = SchoolSerializer(job)
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+
+    def put(self, request, format=None):
+        priority = request.POST.get('priority')
+        school = School.objects.filter(user=request.user).filter(priority=priority)[0]
+        serialized = SchoolSerializer(school, data=request.data, partial=True)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, format=None):
+        priority = request.POST.get('priority')
+        school = School.objects.filter(user=request.user).filter(priority=priority)[0]
+        serialized = JobSerializer(school.delete())
+        return Response(serialized.data, status=status.HTTP_200_OK)
