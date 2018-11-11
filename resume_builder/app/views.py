d28e1d30ffca797037aa47d4d1767bfab5fde2ba
pbdgk <gordonworkspace@gmail.com>
2018-11-11 17:31:43 +0200

    added login required for index route; added redirect to login page

--- resume_builder/app/views.py
+++ resume_builder/app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url='login')
 def index(request):
     context = {}
     return render(request, 'app/index.html', context)
+
+def test_(request):
+    context = {}
+    return render(request, 'app/test_.html', context)
