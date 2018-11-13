7f29ebb1f0573686a26d9e6ed5e255bfbe1d0cea
pbdgk <gordonworkspace@gmail.com>
2018-11-13 11:53:06 +0200

    socials endpoint and view

--- resume_builder/api/urls.py
+++ resume_builder/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ProfileRestView, JobRestView, SchoolRestView, SkillRestView, PhotoRestView
+from .views import ProfileRestView, JobRestView, SchoolRestView, SkillRestView, PhotoRestView, SocialRestView
 
 
 urlpatterns = [
     path('profiles/', ProfileRestView.as_view(), name='profile'),
+    path('socials/', SocialRestView.as_view(), name='social'),
     path('jobs/', JobRestView.as_view(), name='job'),
     path('schools/', SchoolRestView.as_view(), name='school'),
     path('skills/', SkillRestView.as_view(), name='skill'),
--- resume_builder/api/views.py
+++ resume_builder/api/views.py
@@ -6,8 +6,8 @@ from rest_framework import status
 from rest_framework import permissions
 from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser
 
-from app.models import Profile, Job, School, Skill, Photo
-from app.serializers import ProfileSerializer, JobSerializer, SchoolSerializer, SkillSerializer, PhotoSerializer
+from app.models import Profile, Job, School, Skill, Photo, Socials
+from app.serializers import ProfileSerializer, JobSerializer, SchoolSerializer, SkillSerializer, PhotoSerializer, SocialSerializer
 
 
 class ProfileRestView(APIView):
@@ -148,3 +148,23 @@ class PhotoRestView(APIView):
             serialized.save()
             return Response(serialized.data, status=status.HTTP_200_OK)
         return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SocialRestView(APIView):
+
+    def get(self, request, format=None):
+        social = Socials.objects.filter(user=request.user)
+        serialized = SocialSerializer(social, many=True)
+        if serialized:
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        return Response(serialized.errors, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, format=None):
+        name, value = request.data.popitem()
+        social = Socials.objects.filter(user=request.user, field_name=name).first()
+        data = { 'text': value }
+        serialized = SocialSerializer(social, data=data, partial=True)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        return Response(serialized.errors, status=status.HTTP_404_NOT_FOUND)
