bf30628abd370b89ef81fd416f19affa7ef8b456
pbdgk <gordonworkspace@gmail.com>
2018-11-15 21:57:39 +0200

    added summary endpoint; other entdpoints for future dev

--- resume_builder/api/urls.py
+++ resume_builder/api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from .views import ProfileRestView, JobRestView, SchoolRestView, SkillRestView, PhotoRestView, SocialRestView
-
+from . import views
 
 urlpatterns = [
-    path('profiles/', ProfileRestView.as_view(), name='profile'),
-    path('socials/', SocialRestView.as_view(), name='social'),
-    path('jobs/', JobRestView.as_view(), name='job'),
-    path('schools/', SchoolRestView.as_view(), name='school'),
-    path('skills/', SkillRestView.as_view(), name='skill'),
-    path('photos/', PhotoRestView.as_view(), name='photo'),
+    path('profiles/', views.ProfileRestView.as_view(), name='profile'),
+    path('summaries/', views.SummaryRestView.as_view(), name='summary'),
+    path('socials/', views.SocialRestView.as_view(), name='social'),
+    path('jobs/', views.JobRestView.as_view(), name='job'),
+    path('schools/', views.SchoolRestView.as_view(), name='school'),
+    path('skills/', views.SkillRestView.as_view(), name='skill'),
+    path('photos/', views.PhotoRestView.as_view(), name='photo'),
+    path('doc/', views.DocRestView.as_view(), name='doc'),
+    path('download/', views.Download.as_view()),
 ]
