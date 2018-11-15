772c1f0c29be5d0dc162c5766d0326d3b4dc0db2
pbdgk <gordonworkspace@gmail.com>
2018-11-15 21:58:16 +0200

    registered summary in admin

--- resume_builder/app/admin.py
+++ resume_builder/app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Job, School, Skill, Photo, Socials
+from .models import Profile, Job, School, Skill, Photo, Socials, Summary
 
 
-project_models = (Profile, Job, School, Skill, Photo, Socials)
+project_models = (Profile, Job, School, Skill, Photo, Socials, Summary)
 
 for project_model in project_models:
     admin.site.register(project_model)
