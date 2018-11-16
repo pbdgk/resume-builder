e434c06db13332e7831352d25c115707a9f96538
pbdgk <gordonworkspace@gmail.com>
2018-11-16 16:06:18 +0200

    code become unreadable. will try to fix it

--- resume_builder/static/app/js/preview.js
+++ resume_builder/static/app/js/preview.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const PAGE_MARGIN_BOTTOM = 30;
+const PAGE_OFFSET_BOTTOM = 30;
+
 const csrftoken = getCookie("csrftoken");
 const myHeaders = new Headers({
   "X-CSRFToken": csrftoken,
@@ -22,14 +25,13 @@ function getCookie(name) {
   return cookieValue;
 }
 
+
+function ContentContainerWrapException(value) {
+   this.section = value;
+}
+
 function send(e) {
-  // console.log(e)
-  // e.remove()
-  let img = document.querySelector("img");
-  let tmp = img.src;
-  img.src = "/home/g/app/resume_builder/media/photos/photo.png";
   let t = document.documentElement.innerHTML;
-  img.src = tmp;
   const url = "http://127.0.0.1:8000/api/v1/download/";
   fetch(url, {
     method: "POST",
@@ -43,7 +45,6 @@ function send(e) {
       console.log(e);
     });
 }
-// 150 km 1300 - 1400 grn
 
 class Builder {
   constructor(doc) {
@@ -56,8 +57,14 @@ class Builder {
     this.createPage();
     this.buildFirstSection();
     this.buildSummary();
+    this.buildSummary();
+    this.buildSummary();
+    this.buildSummary();
+    this.buildSkills();
+    this.buildSkills();
+    this.buildSkills();
+    this.buildSummary();
     this.buildExperience();
-    this.buildEducation();
     this.buildSkills();
   }
 
@@ -66,6 +73,150 @@ class Builder {
     this.pages.push(page);
     this.container.appendChild(page);
     return page;
+  }
+
+  buildFirstSection() {
+    const section = this.createEntry()
+    const title = this.createNode("div", ["title-col", "first-line-col"]);
+    section.appendChild(title);
+    const { profile, img, socials } = this.doc;
+
+    this.buildHeader(profile, title)
+    this.buildSocials(socials, title);
+    this.buildPhotoSection(img, section)
+  }
+
+  buildHeader(profile, parent){
+    const inner = this.createNode("div", "cRow-inner");
+    parent.appendChild(inner);
+
+    const h1 = this.createNode("h1");
+    const h2 = this.createNode("h2");
+
+    h1.innerHTML = `${profile.first_name} ${profile.last_name}`;
+    h2.innerHTML = profile.profession;
+    inner.appendChild(h1);
+    inner.appendChild(h2);
+  }
+
+  buildSocials(socials, parent) {
+    const innerSocial = this.createNode("div", "cRow-inner");
+    for (let group in socials) {
+      const container = this.createNode("div", ["cCol-inner", "content"]);
+      for (let i = 0; i < socials[group].length; i++) {
+        let social = socials[group][i];
+        const div = this.createNode("div");
+        const strong = this.createNode("strong");
+        strong.innerHTML = social.label;
+        const span = this.createNode("span");
+        span.innerText = social.text;
+        div.appendChild(strong);
+        div.appendChild(span);
+        container.appendChild(div);
+      }
+      innerSocial.appendChild(container);
+    }
+    parent.appendChild(innerSocial);
+  }
+
+  buildPhotoSection(img, parent){
+    const imageContainer = this.createNode("div", [
+      "image-col",
+      "first-line-col"
+    ]);
+    const iContainer = this.createNode("div", "iContainer");
+    const figure = this.createNode("figure");
+    const imgblock = this.createNode("img");
+
+    figure.appendChild(imgblock);
+    iContainer.appendChild(figure);
+    imageContainer.appendChild(iContainer);
+
+    this.getBase64ImageFromUrl(img.image)
+      .then(result => {
+        imgblock.src = result;
+        parent.appendChild(imageContainer)
+      })
+      .catch(err => console.error(err));
+  }
+  
+  buildSummary() {
+    let section = this.createEntry();
+    let contentContainer = this.createDataContainer(section);
+    const ps = this.textToParagraphs(this.doc.summary.text);
+    for (let p of ps) {
+      contentContainer.appendChild(p);
+      if (this.checkEndOfPage(p)){
+        contentContainer = this.manageParapraphWrap(p)
+      }
+    }
+  }
+
+  buildEducation() {
+    const edu = this.doc.edu;
+    let section = this.createTitleEntry("Education");
+    for (let i = 0; i < edu.length; i++) {
+      let school = edu[i];
+
+      let contentContainer = this.createDataContainer(section);
+      
+      this.createDateEntry(contentContainer, school.start, school.end)
+
+      let subject = this.createSubjectEntry(contentContainer, school.name);
+
+      const ps = this.textToParagraphs(school.description);
+      for (let p of ps) {
+        subject.appendChild(p);
+        if (this.checkEndOfPage(subject)) {
+          subject = this.manageSubjectWrap(p);
+        }
+      }
+    }
+  }
+
+  buildExperience() {
+    const exp = this.doc.exp;
+    let section = this.createTitleEntry("Experience");
+    for (let i = 0; i < exp.length; i++) {
+      let job = exp[i];
+
+      let contentContainer = this.createDataContainer(section);
+
+      try {
+        this.createDateEntry(contentContainer, job.start, job.end);
+      } catch (err) {
+        section = err.section;
+      }
+      let subject;
+      try {
+        subject = this.createSubjectEntry(contentContainer, job.position);
+      } catch (err) {
+        section  = err.section;
+        subject = err.subject;
+      }
+      subject = this.createSubTitleEntry(subject, job.company)
+      console.log('sibj', subject)
+      const ps = this.textToParagraphs(job.experience);
+      for (let p of ps) {
+        subject.appendChild(p);
+        if (this.checkEndOfPage(subject)) {
+          subject = this.manageSubjectWrap(p);
+        }
+      }
+    }
+  }
+
+  buildSkills() {
+    const skills = this.doc.skills;
+    let section = this.createTitleEntry("Skills");
+    for (let i = 0; i < skills.length; i++) {
+      let skill = skills[i];
+      const contentContainer = this.createDataContainer(section);
+      this.createRatingEntry(contentContainer, skill);
+      if (this.checkEndOfPage(contentContainer)) {
+        this.manageContentContainerWrap(contentContainer)
+      }
+    }
   }
 
   createNode(name, cls) {
@@ -84,156 +235,6 @@ class Builder {
     const section = this.createNode("div", "section");
     row.appendChild(section);
     return row;
-  }
-
-  buildFirstSection() {
-    const page = this.pages[this.pages.length - 1];
-    const row = this.createSectionSkillet();
-    page.appendChild(row);
-    const section = row.children[0];
-    const title = this.createNode("div", ["title-col", "first-line-col"]);
-    const inner = this.createNode("div", "cRow-inner");
-    const h1 = this.createNode("h1");
-    const h2 = this.createNode("h2");
-
-    const { profile, img, socials } = this.doc;
-    h1.innerHTML = `${profile.first_name} ${profile.last_name}`;
-    inner.appendChild(h1);
-    h2.innerHTML = profile.profession;
-    inner.appendChild(h2);
-
-    title.appendChild(inner);
-    const innerSocial = this.buildSocials(socials);
-
-    title.appendChild(innerSocial);
-
-    const imageContainer = this.createNode("div", [
-      "image-col",
-      "first-line-col"
-    ]);
-    const iContainer = this.createNode("div", "iContainer");
-    const figure = this.createNode("figure");
-    const imgblock = this.createNode("img");
-    imgblock.src = img.image;
-    figure.appendChild(imgblock);
-    iContainer.appendChild(figure);
-    imageContainer.appendChild(iContainer);
-
-    section.appendChild(title);
-    section.appendChild(imageContainer);
-  }
-
-  buildSocials(socials) {
-    const innerSocial = this.createNode("div", "cRow-inner");
-    for (let group in socials) {
-      const container = this.createNode("div", ["cCol-inner", "content"]);
-      for (let i = 0; i < socials[group].length; i++) {
-        let social = socials[group][i];
-        const div = this.createNode("div");
-        const strong = this.createNode("strong");
-        strong.innerHTML = social.label;
-        const span = this.createNode("span");
-        span.innerText = social.text;
-        div.appendChild(strong);
-        div.appendChild(span);
-        container.appendChild(div);
-      }
-      innerSocial.appendChild(container);
-    }
-    return innerSocial;
-  }
-
-  buildSummary() {
-    let section = this.createEntry();
-    let contentContainer = this.createDataContainer(section);
-    const ps = this.textToParagraphs(this.doc.summary.text);
-    for (let p of ps) {
-      contentContainer.appendChild(p);
-      if (this.checkEndOfPage(p)){
-        let page = this.createPage();
-        section = this.createEntry();
-        page.appendChild(section);
-        contentContainer = this.createDataContainer(section);
-        section.appendChild(contentContainer);
-        p.remove()
-        contentContainer.appendChild(p)
-      }
-    }
-  }
-
-  buildEducation() {
-    const edu = this.doc.edu;
-    let section = this.createTitleEntry("Education");
-    for (let i = 0; i < edu.length; i++) {
-      let school = edu[i];
-      let contentContainer = this.createDataContainer(section);
-      this.createDateEntry(contentContainer, school.start, school.end);
-      let subject = this.createSubjectEntry(contentContainer, school.name);
-      const ps = this.textToParagraphs(school.description);
-      for (let p of ps) {
-        subject.appendChild(p);
-        if (this.checkEndOfPage(p)) {
-          p.remove();
-          this.createPage();
-          section = this.createEntry(null, true);
-          contentContainer = this.createDataContainer(section);
-          subject = this.createWrappedSubjectEntry(contentContainer);
-          subject.appendChild(p);
-        }
-      }
-    }
-  }
-
-  buildExperience() {
-    const exp = this.doc.exp;
-    let section = this.createTitleEntry("Experience");
-    for (let i = 0; i < exp.length; i++) {
-      let job = exp[i];
-      let contentContainer = this.createDataContainer(section);
-      this.createDateEntry(contentContainer, job.start, job.end);
-      let subject = this.createSubjectEntry(contentContainer, job.position);
-      const ps = this.textToParagraphs(job.experience);
-      for (let p of ps) {
-        subject.appendChild(p);
-        if (this.checkEndOfPage(p)) {
-          p.remove();
-          this.createPage();
-          section = this.createEntry();
-          console.log(section)
-          contentContainer = this.createDataContainer(section);
-          subject = this.createWrappedSubjectEntry(contentContainer);
-          subject.appendChild(p);
-        }
-      }
-    }
-  }
-
-  buildSkills() {
-    const skills = this.doc.skills;
-    let section = this.createTitleEntry("Skills");
-    for (let i = 0; i < skills.length; i++) {
-      let skill = skills[i];
-      const contentContainer = this.createDataContainer(section);
-      this.createRatingEntry(contentContainer, skill);
-      if (this.checkEndOfPage(contentContainer)) {
-        let page = this.createPage();
-        section = this.createEntry();
-        page.appendChild(section);
-        contentContainer.remove();
-        section.appendChild(contentContainer);
-      }
-    }
-  }
-
-  checkEndOfPage(element) {
-    let lastPage = this.getLastPage();
-    const y_page = lastPage.getBoundingClientRect().bottom - 30 - 30;
-    const y_e = element.getBoundingClientRect().bottom;
-    // return y_e > y_page
-    if (y_e > y_page) {
-      console.log(element, y_page, y_e);
-      return true;
-    }
   }
 
   createRatingEntry(contentContainer, skill) {
@@ -261,13 +262,36 @@ class Builder {
     dateTo.innerHTML = end;
 
     if (this.checkEndOfPage(contentContainer)) {
-      let page = this.createPage();
-      let section = this.createEntry();
-      page.appendChild(section);
-      contentContainer.remove();
-      section.appendChild(contentContainer);
+      console.log('!!!!!', contentContainer)
+      this.manageContentContainerWrap(contentContainer);
     }
   }
+
+  createSubTitleEntry(subject, companyName){
+    const companyTitle = this.createNode('p', 'subtitle')
+    companyTitle.innerHTML = companyName;
+    subject.appendChild(companyTitle)
+    if (this.checkEndOfPage(companyTitle)){
+      return this.manageSubjectWrap(companyTitle)
+    }
+    return subject;
+  }
+
+  // createSubjectWithSubtitleEntry(contentContainer, companyName, text){
+  //   try{
+  //     let subject = this.createSubjectEntry(contentContainer, text)
+  //   } finally{
+
+  //   }
+  //     const companyTitle = this.createNode('p')
+  //     companyTitle.innerHTML = companyName;
+  //     subject.appendChild(companyTitle)
+  //     if (this.checkEndOfPage(subject)){
+  //       console.log('sfdsfsd')
+  //       this.manageContentContainerWrap(subject)
+  //     }
+  //     return subject;
+  // }
 
   createSubjectEntry(contentContainer, text) {
     const subject = this.createNode("div", ["subject", "inline-b"]);
@@ -276,13 +300,40 @@ class Builder {
     subject.appendChild(name);
     name.innerHTML = text;
     if (this.checkEndOfPage(contentContainer)) {
-      let page = this.createPage();
-      let section = this.createEntry();
-      page.appendChild(section);
-      contentContainer.remove();
-      section.appendChild(contentContainer);
+      try {
+        this.manageContentContainerWrap(contentContainer);
+      } catch (err){
+        err.subject = subject;
+        throw err
+      }
     }
     return subject;
+  }
+
+
+  manageParapraphWrap(p){
+    this.createPage();
+    const section = this.createEntry();
+    const contentContainer = this.createDataContainer(section);
+    this._removeTo(p, contentContainer)
+    return contentContainer;
+  }
+
+  manageSubjectWrap(el){
+    this.createPage()
+    const section = this.createEntry()
+    const contentContainer = this.createDataContainer(section)
+    let subject = this.createWrappedSubjectEntry(contentContainer)
+    this._removeTo(el, subject)
+    return subject;
+  }
+
+  manageContentContainerWrap(contentContainer){
+      this.createPage();
+      const section = this.createEntry();
+      this._removeTo(contentContainer, section);
+      throw new ContentContainerWrapException(section);
+
   }
 
   createWrappedSubjectEntry(contentContainer) {
@@ -295,22 +346,30 @@ class Builder {
     contentContainer.appendChild(subject);
     return subject;
   }
+
+  checkEndOfPage(element) {
+    let lastPage = this.getLastPage();
+
+    const y_page = lastPage.getBoundingClientRect().bottom - PAGE_MARGIN_BOTTOM - PAGE_OFFSET_BOTTOM;
+    const y_element = element.getBoundingClientRect().bottom;
+    // console.log(y_element, y_page, element) 
+    if (y_element > y_page){
+      console.log(y_element, y_page, element) 
+    }
+    return y_element > y_page
+  }
   getLastPage() {
     return this.pages[this.pages.length - 1];
   }
 
   createTitleEntry(name) {
-    const row = this.createSectionSkillet();
-    let page = this.getLastPage();
-    page.appendChild(row);
-    const section = row.children[0];
+    const section = this.createEntry()
     const title = this.createNode("div", "title");
     section.appendChild(title);
     title.innerHTML = name;
-    if (this.checkEndOfPage(row)) {
-      row.remove();
-      page = this.createPage();
-      page.appendChild(row);
+    if (this.checkEndOfPage(section)) {
+      const page = this.createPage();
+      this._removeTo(section.parentNode, page)
     }
     return section;
   }
@@ -331,9 +390,6 @@ class Builder {
   }
 
   textToParagraphs(text) {
-    if (!text) {
-      return false;
-    }
     const paragraphs = [];
     const pArr = text.split("\n");
     for (let i = 0; i < pArr.length; i++) {
@@ -343,11 +399,37 @@ class Builder {
     }
     return paragraphs;
   }
+
+  async getBase64ImageFromUrl(imageUrl) {
+    var res = await fetch(imageUrl);
+    var blob = await res.blob();
+
+    return new Promise((resolve, reject) => {
+      var reader = new FileReader();
+      reader.addEventListener("load", function () {
+        resolve(reader.result);
+      }, false);
+
+      reader.onerror = () => {
+        return reject(this);
+      };
+      reader.readAsDataURL(blob);
+    })
+  }
+
+  _removeTo(el, parent){
+    el.remove();
+    parent.appendChild(el);
+  }
 }
 
 getData("doc").then(doc => {
-  const builder = new Builder(doc);
-  builder.build();
+  try {
+    const builder = new Builder(doc);
+    builder.build();
+  } catch (err) {
+    console.error(err)
+  }
 });
 
 function getData(cName) {
